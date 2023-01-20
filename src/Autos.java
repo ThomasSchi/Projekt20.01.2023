c75e37efdfcfe34af5f80155f43a44953e989887
ThomasSchi <thomasschilinski@yahoo.de>
2023-01-20 10:47:03 +0100

    First Commit

--- src/Autos.java
+++ src/Autos.java
@@ -2,10 +2,46 @@
 public class Autos {
     private String marke;
     private String typ;
-    private  double preisprotag ;
+    private double preisprotag;
     private boolean verfügbarkeit;
 
+    public Autos(String marke, String typ, double preisprotag, boolean verfügbarkeit) {
+        this.marke = marke;
+        this.typ = typ;
+        this.preisprotag = preisprotag;
+        this.verfügbarkeit = verfügbarkeit;
+    }
 
+    public String getMarke() {
+        return marke;
+    }
 
+    public void setMarke(String marke) {
+        this.marke = marke;
+    }
+
+    public String getTyp() {
+        return typ;
+    }
+
+    public void setTyp(String typ) {
+        this.typ = typ;
+    }
+
+    public double getPreisprotag() {
+        return preisprotag;
+    }
+
+    public void setPreisprotag(double preisprotag) {
+        this.preisprotag = preisprotag;
+    }
+
+    public boolean isVerfügbarkeit() {
+        return verfügbarkeit;
+    }
+
+    public void setVerfügbarkeit(boolean verfügbarkeit) {
+        this.verfügbarkeit = verfügbarkeit;
+    }
 }
 
