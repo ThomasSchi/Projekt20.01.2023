9478d87cf2da9daab2556fc0c118aa8cd53533d9
khoi.hoang <khoi.hoang@accenture.com>
2023-01-20 13:30:10 +0100

    added Lagerbestand

--- src/Lagerbestand.java
+++ src/Lagerbestand.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
 public class Lagerbestand {
+    private int Sollbestand;
+    private int Istbestand;
+        ArrayList<Autos> availableCars;
+        ArrayList<Autos> rentedCars;
+
+        public Lagerbestand() {
+            availableCars = new ArrayList<>();
+            rentedCars = new ArrayList<>();
+        }
+
+        public void addCar(Autos car) {
+            if(car.getVerfuegbarkeit()==true){
+                availableCars.add(car);
+            }else{
+                rentedCars.add(car);
+            }
+        }
+
+        public void removeCar(ArrayList<Autos> cars, Autos auto) {
+            for (Autos car:cars
+                 ) {
+                if (auto.getId() == car.getId()) {
+                    cars.remove(car);
+                    break;
+                }
+            }
+        }
+
+        public ArrayList<Autos> checkAvailability() {
+            return availableCars;
+        }
+
+        public ArrayList<Autos> checkRentedCars() {
+            return rentedCars;
+        }
+
+
 }
