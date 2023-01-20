07a62ace77f059f081135b0f3385ac803dd45893
ThomasSchi <thomasschilinski@yahoo.de>
2023-01-20 10:40:18 +0100

    First Commit

---
+++ src/Autos.java
@@ -0,0 +1,11 @@
+
+public class Autos {
+    private String marke;
+    private String typ;
+    private  double preisprotag ;
+    private boolean verfügbarkeit;
+
+
+
+}
+
---
+++ src/Autovermietung.java
@@ -0,0 +1,5 @@
+public class Autovermietung {
+    public static void main(String[] args) {
+
+    }
+}
