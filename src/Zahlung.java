8f898fa5129a826c0bb61a6a0fe52d52d4c8bf80
amiracon <contichamira@gmail.com>
2023-01-20 13:24:37 +0100

    hi

--- src/Zahlung.java
+++ src/Zahlung.java
@@ -2,6 +2,7 @@ public class Zahlung {
     private double betrag;
     private boolean zahlungsstatus;
     private Mietvertrag vermietung;
+    private int Vertragsnummer;
 
     public Zahlung(double betrag, boolean zahlungsstatus, Mietvertrag vermietung) {
         this.betrag = betrag;
@@ -9,15 +10,18 @@ public class Zahlung {
         this.vermietung = vermietung;
     }
 
-    public double getBetrag() {
-        return betrag;
+    public double getBetrag(double betrag) {
+
+        return betrag ;
     }
 
     public void setBetrag(double betrag) {
         this.betrag = betrag;
     }
 
-    public boolean isZahlungsstatus() {
+    public boolean isZahlungsstatus(boolean bezahlt) {
+
+
         return zahlungsstatus;
     }
 
@@ -31,5 +35,17 @@ public class Zahlung {
 
     public void setVermietung(Mietvertrag vermietung) {
         this.vermietung = vermietung;
+    }
+
+    public void erstelleZahlungsbestaetigung() {
+        if (this.zahlungsstatus) {
+            String bestaetigung = "Zahlungsbestätigung\n" +
+                    "Betrag: " + this.betrag + "\n" +
+                    "Mietvertrag: " + //this.vermietung.getVertragsnummer() + "\n" +
+                    "Bezahlt: Ja";
+            System.out.println(bestaetigung);
+        } else {
+            System.out.println("Zahlung noch nicht eingegangen");
+        }
     }
 }
